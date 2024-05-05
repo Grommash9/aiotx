862926625aee91b33c0e2a63c6869c1dd45905f3
Oleksandr Prudnikov <prudnikov21@icloud.com>
2024-05-05 16:00:55 +0100

    send working with UTXO now

--- aiotx/clients/_utxo_base_client.py
+++ aiotx/clients/_utxo_base_client.py
@@ -76,25 +76,26 @@ class AioTxUTXOClient(AioTxClient):
         return result["result"]
     
     async def send(self, private_key: str, to_address: str, amount: float, fee: float) -> str:
-        from_address = self.get_address_from_private_key(private_key)
+        from_wallet = self.get_address_from_private_key(private_key)
+        from_address = from_wallet["address"]
         # # Получаем неизрасходованные выходы для адреса отправителя
-        # unspent_outputs = await self.get_unspent_outputs(from_address)
+        utxo_list = await self.monitor._get_utxo_data(from_address)
 
-        # # Выбираем входы и суммируем их значения
-        # inputs = []
-        # total_value = 0
-        # for output in unspent_outputs:
-        #     input_data = (output['txid'], output['vout'], output['amount'])
-        #     inputs.append(input_data)
-        #     total_value += output['amount']
-        #     if total_value >= amount + fee:
-        #         break
-        total_value = self.to_satoshi(1.5)
-        inputs = [("737bef3e8161d15e70f4b230d433f40fb3b5bb197a962289047de12ed9900bb4", 0, total_value)]
+        # Выбираем входы и суммируем их значения
+        inputs = []
+        total_value = 0
+        for utxo in utxo_list:
+            input_data = (utxo[0], utxo[1], utxo[2])
+            inputs.append(input_data)
+            total_value += utxo[2]
+            if total_value >= amount + fee:
+                break
+        # total_value = self.to_satoshi(1.5)
+        # inputs = [("737bef3e8161d15e70f4b230d433f40fb3b5bb197a962289047de12ed9900bb4", 0, total_value)]
 
         outputs = [
             (to_address, amount),
-            (from_address["address"], total_value - amount - fee)
+            (from_address, total_value - amount - fee)
         ]
         raw_transaction = await self.create_transaction(inputs, outputs, [private_key])
 
@@ -174,8 +175,14 @@ class UTXOMonitor(BlockMonitor):
 
         addresses = await self._get_addresses()
         for transaction in block_data["tx"]:
-            # if transaction["txid"] == "737bef3e8161d15e70f4b230d433f40fb3b5bb197a962289047de12ed9900bb4":
-            #     breakpoint()
+
+            for input_utxo in transaction["vin"]:
+                txid = input_utxo.get("txid")
+                vout = input_utxo.get("vout")
+                if txid is None or vout is None:
+                    continue
+                await self._process_input_utxo(txid, vout)
+
 
             for output in transaction["vout"]:
                 outputs_scriptPubKey = output.get("scriptPubKey")
@@ -210,11 +217,15 @@ class UTXOMonitor(BlockMonitor):
 
                 # Создание таблицы для адресов
                 await c.execute(f'''CREATE TABLE IF NOT EXISTS {self._addresses_table_name}
-                                    (uuid TEXT PRIMARY KEY, address TEXT, block_number INTEGER)''')
+                                    (address TEXT PRIMARY KEY, block_number INTEGER)''')
 
                 # Создание таблицы для UTXO
                 await c.execute(f'''CREATE TABLE IF NOT EXISTS {self._utxo_table_name}
-                                    (address TEXT, tx_id TEXT, amount_satoshi INTEGER, output_n INTEGER) ''')
+                                    (address TEXT,
+                                    tx_id TEXT,
+                                    amount_satoshi INTEGER,
+                                    output_n INTEGER,
+                                    PRIMARY KEY (tx_id, output_n))''')
 
             await conn.commit()
 
@@ -248,7 +259,7 @@ class UTXOMonitor(BlockMonitor):
                         raise e
 
     async def _add_new_address(self, address: str, block_number: int):
-        query = f'''INSERT INTO {self._addresses_table_name}
+        query = f'''INSERT OR REPLACE INTO {self._addresses_table_name}
                     (address, block_number) VALUES (?, ?)'''
         await self._run_query(query, (address, block_number))
         last_known_block = await self._get_last_block()
@@ -256,22 +267,42 @@ class UTXOMonitor(BlockMonitor):
             await self._update_last_block(block_number)
 
     async def _add_new_utxo(self, address: str, tx_id: str, amount: int, output_n: int) -> None:
-        query = f'''INSERT INTO {self._utxo_table_name}
+        query = f'''INSERT OR IGNORE INTO {self._utxo_table_name}
                     (address, tx_id, amount_satoshi, output_n) VALUES (?, ?, ?, ?)'''
         await self._run_query(query, (address, tx_id, amount, output_n))
 
     async def _update_last_block(self, block_number: int) -> None:
         query = f'''UPDATE {self._last_block_table_name} SET block_number = ?'''
         await self._run_query(query, (block_number,))
+    
+    async def _process_input_utxo(self, txid: str, vout: int):
+        utxo = await self._get_utxo(txid, vout)
+        if utxo:
+            await self._delete_utxo(txid, vout)
 
     async def _get_utxo_data(self, address: str):
-        query = f'''SELECT utxo.tx_id, utxo.amount, utxo.output_n
+        query = f'''SELECT utxo.tx_id, utxo.output_n, utxo.amount_satoshi
                     FROM {self._utxo_table_name} utxo
                     WHERE utxo.address = ?'''
         async with aiosqlite.connect(self._db_name) as conn:
             async with conn.cursor() as c:
                 await c.execute(query, (address,))
                 return await c.fetchall()
+            
+    async def _get_utxo(self, tx_id: str, output_n: int) -> Optional[tuple]:
+        query = f'''SELECT utxo.tx_id, utxo.output_n
+                    FROM {self._utxo_table_name} utxo
+                    WHERE utxo.tx_id = ? and output_n = ?'''
+        async with aiosqlite.connect(self._db_name) as conn:
+            async with conn.cursor() as c:
+                await c.execute(query, (tx_id, output_n))
+                return await c.fetchone()
+            
+    async def _delete_utxo(self, tx_id: str, output_n: int):
+        query = f'''DELETE FROM {self._utxo_table_name}
+                    WHERE tx_id = ? AND output_n = ?'''
+        await self._run_query(query, (tx_id, output_n))
+
 
     async def _get_last_block(self) -> Optional[int]:
         query = f'''SELECT block_number FROM {self._last_block_table_name}'''
