b86246240266de35e8e530504f34566b7ddfd656
Oleksandr Prudnikov <prudnikov21@icloud.com>
2024-05-11 13:37:47 +0100

    remove bulk send for now

--- tests/test_utxo/test_btc/test_btc_client.py
+++ tests/test_utxo/test_btc/test_btc_client.py
@@ -22,17 +22,17 @@ async def test_send_transaction(btc_client: AioTxBTCClient):
     await btc_client.monitor._delete_utxo("25d56f693d5c4d00d3f98e58c8bd66e8db930e38c8a556bd67737b66cbf31ab9", 0)
 
 
-@vcr_c.use_cassette("ltc/send_bulk_legacy_and_segwit_tx.yaml")
-async def test_bulk_send_transaction(btc_client: AioTxBTCClient):
-    fee = btc_client.to_satoshi(0.00001)
-    await btc_client.monitor._add_new_address(TEST_BTC_ADDRESS)
-    await btc_client.monitor._add_new_utxo(TEST_BTC_ADDRESS,
-                                            "4f9a64d28ae22134379f7da50282d7ca2ead8a1f8378b20e25defcaaa5c59228", 14263, 0)
+# @vcr_c.use_cassette("ltc/send_bulk_legacy_and_segwit_tx.yaml")
+# async def test_bulk_send_transaction(btc_client: AioTxBTCClient):
+#     fee = btc_client.to_satoshi(0.00001)
+#     await btc_client.monitor._add_new_address(TEST_BTC_ADDRESS)
+#     await btc_client.monitor._add_new_utxo(TEST_BTC_ADDRESS,
+#                                             "4f9a64d28ae22134379f7da50282d7ca2ead8a1f8378b20e25defcaaa5c59228", 14263, 0)
     
-    amount = 1000 - fee / 2
-    tx_id = await btc_client.send_bulk(TEST_BTC_WALLET_PRIVATE_KEY,
-                                        {"tltc1q24gng65qj3wr55878324w2eeeta4k2plfwaf54": amount,
-                                         "2N5GkkPqPNYVVRibjxPBpPgAPoxfVh16eax": amount,
-                                         TEST_BTC_ADDRESS: amount}, fee)
+#     amount = 1000 - fee / 2
+#     tx_id = await btc_client.send_bulk(TEST_BTC_WALLET_PRIVATE_KEY,
+#                                         {"tltc1q24gng65qj3wr55878324w2eeeta4k2plfwaf54": amount,
+#                                          "2N5GkkPqPNYVVRibjxPBpPgAPoxfVh16eax": amount,
+#                                          TEST_BTC_ADDRESS: amount}, fee)
     
-    assert tx_id == "64a89e7e269469c126e96d1de7b553850716c71d9081b153429ff781758a59a1"
+#     assert tx_id == "64a89e7e269469c126e96d1de7b553850716c71d9081b153429ff781758a59a1"
