a17fc0b0270e6270a8408cfbdafe30adc5e0dadd
Oleksandr Prudnikov <prudnikov21@icloud.com>
2024-05-20 06:38:39 +0100

    mysql tests pipeline and creating UTXO on transaction send

--- tests/test_utxo/test_btc/test_btc_client.py
+++ tests/test_utxo/test_btc/test_btc_client.py
@@ -29,7 +29,10 @@ async def test_send_transaction(btc_client: AioTxBTCClient):
         TEST_BTC_WALLET_PRIVATE_KEY, "tb1q4tf08gc2vf8dgfqtd3jen3s5tddz6uu0mnyf90", amount, fee
     )
     utxo_list = await btc_client.monitor._get_utxo_data(TEST_BTC_ADDRESS)
-    assert len(utxo_list) == 0
+    utxo_list = await btc_client.monitor._get_utxo_data(TEST_BTC_ADDRESS)
+    assert len(utxo_list) == 1
+    assert utxo_list[0][0] == "4f9a64d28ae22134379f7da50282d7ca2ead8a1f8378b20e25defcaaa5c59228"
+    assert utxo_list[0][2] == 14263
     assert tx_id == "4f9a64d28ae22134379f7da50282d7ca2ead8a1f8378b20e25defcaaa5c59228"
     await btc_client.monitor._delete_utxo("25d56f693d5c4d00d3f98e58c8bd66e8db930e38c8a556bd67737b66cbf31ab9", 0)
 
@@ -57,7 +60,9 @@ async def test_send_to_all_address_types(btc_client: AioTxBTCClient):
         fee,
     )
     utxo_list = await btc_client.monitor._get_utxo_data(TEST_BTC_ADDRESS)
-    assert len(utxo_list) == 0
+    assert len(utxo_list) == 1
+    assert utxo_list[0][0] == "35158b7a8b6057bc67f6d904c64b5986adea8260f0bc96cbd755b530878e3cc2"
+    assert utxo_list[0][2] == 6263
     assert tx_id == "35158b7a8b6057bc67f6d904c64b5986adea8260f0bc96cbd755b530878e3cc2"
 
 
@@ -79,9 +84,13 @@ async def test_send_with_auto_fee(btc_client: AioTxBTCClient):
         conf_target=1,
         estimate_mode=FeeEstimate.CONSERVATIVE,
     )
-    utxo_list = await btc_client.monitor._get_utxo_data(TEST_BTC_ADDRESS)
-    assert len(utxo_list) == 0
+    
     assert tx_id == "77cb20c4b0325242b9e5f45f4850e5387dc585d6b72bb36ba65a126534436973"
+
+    utxo_list = await btc_client.monitor._get_utxo_data(TEST_BTC_ADDRESS)
+    print("utxo_list", utxo_list)
+    assert len(utxo_list) == 0
+
 
 
 
