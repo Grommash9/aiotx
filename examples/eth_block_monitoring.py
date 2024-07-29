877569636896c968b3e9740018d5adf015961c23
Oleksandr Prudnikov <67575598+Grommash9@users.noreply.github.com>
2024-07-29 18:35:09 +0100

    examples updates (#128)

--- examples/eth_block_monitoring.py
+++ examples/eth_block_monitoring.py
@@ -23,8 +23,6 @@ async def handle_transaction(transaction):
 
 async def main():
     await eth_client.start_monitoring()
-    while True:
-        await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
